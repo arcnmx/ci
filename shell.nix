5306e8dd1202683fd01ab8145f97b656c5cafcc7
arcnmx <arcnmx@users.noreply.github.com>
2019-10-06 16:45:13 -0700

    test alias

--- shell.nix
+++ shell.nix
@@ -6,10 +6,17 @@
 
   shellHook = ''
     export NIX_PATH="ci=$CI_ROOT:$NIX_PATH"
+    CI_CONFIG_FILES=($CI_CONFIG_ROOT/tests/* $CI_CONFIG_ROOT/examples/ci.nix)
 
     gh-actions-generate() {
-      for f in $CI_CONFIG_ROOT/tests/* $CI_CONFIG_ROOT/examples/ci.nix; do
+      for f in ''${CI_CONFIG_FILES[*]}; do
         nix run --arg config $f ci.run.gh-actions-generate
+      done
+    }
+
+    test-all() {
+      for f in ''${CI_CONFIG_FILES[*]}; do
+        nix run --arg config $f ci.test || break
       done
     }
   '';
