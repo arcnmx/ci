b21325ebd48adc4613d6990d57bfaa1b5273f0a5
arcnmx <arcnmx@users.noreply.github.com>
2019-10-07 18:46:34 -0700

    oops that's necessary for caching

--- nix/lib/build/cache.sh
+++ nix/lib/build/cache.sh
@@ -3,6 +3,8 @@ set -eu
 
 CI_CACHE_LIST=($(cat))
 
+source $CI_BUILD_ATTRS
+
 if [[ ${#CI_CACHE_LIST[@]} -gt 0 && -n ${CACHIX_SIGNING_KEY-} && -n ${CACHIX_CACHE-} ]]; then
   echo ${CI_CACHE_LIST[*]} | @cachix@/bin/cachix push "$CACHIX_CACHE" || true
 fi
