c523e8e9b4af6e95f8981a8488e026093c7a9afa
arcnmx <arcnmx@users.noreply.github.com>
2020-09-11 10:12:03 -0700

    filter out cached paths???

--- nix/lib/build/cache.sh
+++ nix/lib/build/cache.sh
@@ -6,5 +6,11 @@ CI_CACHE_LIST=($(cat))
 source $CI_BUILD_ATTRS
 
 if [[ ${#CI_CACHE_LIST[@]} -gt 0 && -n ${CACHIX_SIGNING_KEY-} && -n ${CACHIX_CACHE-} ]]; then
-  echo ${CI_CACHE_LIST[*]} | @cachix@/bin/cachix push "$CACHIX_CACHE" || true
+  FILTERED=()
+  for path in "${CI_CACHE_LIST[@]}"; do
+    if [[ -e $path ]]; then
+      FILTERED+=("$path")
+    fi
+  done
+  echo ${FILTERED[*]} | @cachix@/bin/cachix push "$CACHIX_CACHE" || true
 fi
