7259add9749b2d10368f8d6d1599820f2fb66816
arcnmx <arcnmx@users.noreply.github.com>
2021-05-14 13:32:53 -0700

    I know I'm lazy but please .-.

--- shell.nix
+++ shell.nix
@@ -9,14 +9,14 @@
     CI_CONFIG_FILES=($CI_CONFIG_ROOT/tests/* $CI_CONFIG_ROOT/examples/ci.nix)
 
     gh-actions-generate() {
-      for f in ''${CI_CONFIG_FILES[*]}; do
-        nix run --arg config $f ci.run.gh-actions-generate
+      for f in "''${CI_CONFIG_FILES[@]}"; do
+        nix run --arg config "$f" ci.run.gh-actions-generate
       done
     }
 
     test-all() {
-      for f in ''${CI_CONFIG_FILES[*]}; do
-        nix run --arg config $f ci.test || break
+      for f in "''${CI_CONFIG_FILES[@]}"; do
+        nix run --arg config "$f" ci.test || break
       done
     }
   '';
