68d7fdb9ad5228f253f8e0901575b66d262a358a
arcnmx <arcnmx@users.noreply.github.com>
2022-01-28 11:08:26 -0800

    nix 2.6.0

--- nix/lib/cipkgs.nix
+++ nix/lib/cipkgs.nix
@@ -6,6 +6,7 @@ rec {
   };
   nixpkgsFor = {
     # pinned nixpkgs evaluations bundled with nix binary releases (https://hydra.nixos.org/project/nix)
+    "2.6.0" = nixpkgsFor."2.5.1";
     "2.5.1" = nixpkgsFor."2.5.0";
     "2.5.0" = nixpkgsFor."2.4";
     "2.4" = nixpkgsSource {
