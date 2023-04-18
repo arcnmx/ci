130bb716c053490a5a5528c04957fe3bbab4501a
arcnmx <git@git.arcn.mx>
2023-04-18 07:23:24 -0700

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,23 +1,29 @@
-{ pkgs ? import <nixpkgs> { } }: with pkgs; mkShell {
-  CI_ROOT = toString ./.;
-  CI_CONFIG_ROOT = toString ./.;
+{ pkgs ? import <nixpkgs> { } }: with pkgs; let
+  CI_CONFIG_ROOT = "\${CI_CONFIG_ROOT-${toString ./.}}";
+  CI_CONFIG_FILES = "${CI_CONFIG_ROOT}/tests/*";
+  gh-actions-generate = writeShellScriptBin "gh-actions-generate" ''
+    for f in ${CI_CONFIG_FILES}; do
+      nix run --arg config $f -f ${CI_CONFIG_ROOT} run.gh-actions-generate
+    done
+  '';
+  test-all = writeShellScriptBin "test-all" ''
+    for f in ${CI_CONFIG_FILES}; do
+      if ! nix run --arg config $f -f ${CI_CONFIG_ROOT} test; then
+        echo failed test $f >&2
+        exit 1
+      fi
+    done
+  '';
+in mkShell {
   #CI_CONFIG = toString ./example/ci.nix
   CI_PLATFORM = "impure"; # use host's nixpkgs for more convenient testing
 
+  nativeBuildInputs = [
+    gh-actions-generate
+    test-all
+  ];
   shellHook = ''
-    export NIX_PATH="ci=$CI_ROOT:$NIX_PATH"
-    CI_CONFIG_FILES=($CI_CONFIG_ROOT/tests/*)
-
-    gh-actions-generate() {
-      for f in "''${CI_CONFIG_FILES[@]}"; do
-        nix run --arg config "$f" ci.run.gh-actions-generate
-      done
-    }
-
-    test-all() {
-      for f in "''${CI_CONFIG_FILES[@]}"; do
-        nix run --arg config "$f" ci.test || break
-      done
-    }
+    export CI_ROOT=''${CI_ROOT-${toString ./.}}
+    export CI_CONFIG_ROOT=${CI_CONFIG_ROOT}
   '';
 }
