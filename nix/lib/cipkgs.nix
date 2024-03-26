a432559ad0994a0193c0971b341bdcf1ecbdb6c7
arcnmx <git@git.arcn.mx>
2024-03-26 13:56:19 -0700

    bump cipkgs

--- nix/lib/cipkgs.nix
+++ nix/lib/cipkgs.nix
@@ -14,7 +14,8 @@ rec {
   };
   nixpkgsFor = {
     # pinned nixpkgs evaluations bundled with nix binary releases (https://github.com/NixOS/nix/blob/master/flake.lock)
-    "2.21." = nixpkgsFor."2.20.0";
+    "2.21." = nixpkgsFor."2.21.1";
+    "2.21.1" = nixpkgsFor."2.21.0";
     "2.21.0" = nixpkgsSource {
       rev = "b550fe4b4776908ac2a861124307045f8e717c8e";
       sha256 = "sha256-7kkJQd4rZ+vFrzWu8sTRtta5D1kBG0LSRYAfhtmMlSo=";
